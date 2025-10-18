843db4b8782460b3796e42b29d15c18b7cf08d60
Dinuka-SID <bandarigodadm.21@uom.lk>
2025-10-19 00:05:01 +0530

    server.ts modified

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,6 +2,6 @@ import app from "./app";
 import { ENV } from "./config/env";
 import { logger } from "./utils/logger";
 
-app.listen(ENV.PORT, () => {
-    logger.info(`Server running on http://localhost:${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+app.listen(ENV.PORT, "0.0.0.0", () => {
+    logger.info(`Server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
 });
